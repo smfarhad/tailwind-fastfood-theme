afa8c2b0115a71206b3a7f6852a2f0b4cdfbe826
smfarhad <farhad1556@gmail.com>
2024-04-18 08:10:15 +0600

    menu wp

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: '#010a5e',
+        primaryColorLight: "#010d78",
+        secondaryColor: '#FFCC00',
+        paragraphColor: '#c0c0c0',
+        whiteColor: '#fff',
+        blackColor: "#000",
+        greenColor: "#007936",
+        redColor: "#cc3433",
+        darkColor: '#000',
+        darkColorLight: "#171717",
+      }
+    },
+    fontFamily: {
+      oswald: ['Oswald', 'sans-serif'],
+      dmsans: ['DM Sans', 'sans-serif']
+    }
   },
   plugins: [],
 }
