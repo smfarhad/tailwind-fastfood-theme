23915a1d0ab696073fab483d8089dfa335e2b8df
smfarhad <farhad1556@gmail.com>
2024-04-21 14:05:33 +0600

    animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { transform } = require('sucrase');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
@@ -19,8 +21,17 @@ module.exports = {
         redColor: "#cc3433",
         darkColor: '#000',
         darkColorLight: "#171717",
+      },
+      keyframes:{
+        move:{
+          "50%":{transform:'translateY(-1rem)'}
+        }
+      },
+      animation:{
+        "movingY":'move 2s linear infinite'
       }
     },
+    
     fontFamily: {
       oswald: ['Oswald', 'sans-serif'],
       dmsans: ['DM Sans', 'sans-serif']
